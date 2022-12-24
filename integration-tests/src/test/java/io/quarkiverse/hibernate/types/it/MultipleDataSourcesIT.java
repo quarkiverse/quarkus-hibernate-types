6e1fc89bee7aad0b3ab2566ce67d9def37639dab
w.glanzer <w.glanzer@conceptive.io>
2022-12-24 11:42:53 +0100

    chore: replaced "NativeImageTest" with "QuarkusIntegrationTest"

--- integration-tests/src/test/java/io/quarkiverse/hibernate/types/it/MultipleDataSourcesIT.java
+++ integration-tests/src/test/java/io/quarkiverse/hibernate/types/it/MultipleDataSourcesIT.java
@@ -1,8 +1,8 @@
 package io.quarkiverse.hibernate.types.it;
 
-import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@NativeImageTest
+@QuarkusIntegrationTest
 public class MultipleDataSourcesIT extends MultipleDataSourcesTest {
 
 }
