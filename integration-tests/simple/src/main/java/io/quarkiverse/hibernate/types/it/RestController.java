b744ca0ae61c2df6bc2077a6a53a43aece9c01f6
w.glanzer <w.glanzer@conceptive.io>
2023-04-12 21:33:26 +0200

    chore: upgraded to quarkus 3.0

--- integration-tests/simple/src/main/java/io/quarkiverse/hibernate/types/it/RestController.java
+++ integration-tests/simple/src/main/java/io/quarkiverse/hibernate/types/it/RestController.java
@@ -4,13 +4,13 @@ import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.EntityManager;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.persistence.EntityManager;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import io.quarkiverse.hibernate.types.it.entities.MyEntity;
 import io.quarkus.arc.Arc;
